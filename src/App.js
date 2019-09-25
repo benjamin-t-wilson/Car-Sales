e8e20db42bfdabe61cafbc3e32c66e43d166bf73
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-09-24 18:34:27 -0700

    switched app to use redux for state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Header from "./components/Header";
+import AddedFeatures from "./components/AddedFeatures";
+import AdditionalFeatures from "./components/AdditionalFeatures";
+import Total from "./components/Total";
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+//import connect from react-redux as it will affect both. This allows React to access information from Redux
+import { connect } from "react-redux";
+
+//Added props to function to be used by mapStateToProps
+const App = props => {
+  //Initial state commented out so I can use Redux
+  // const state = {
+  //   additionalPrice: 0,
+  //   car: {
+  //     price: 26395,
+  //     name: '2019 Ford Mustang',
+  //     image:
+  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+  //     features: []
+  //   },
+  //   store: [
+  //     { id: 1, name: 'V-6 engine', price: 1500 },
+  //     { id: 2, name: 'Racing detail package', price: 1500 },
+  //     { id: 3, name: 'Premium sound system', price: 500 },
+  //     { id: 4, name: 'Rear spoiler', price: 250 }
+  //   ]
+  // };
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
@@ -31,18 +36,32 @@ const App = () => {
     // dipsatch an action here to add an item
   };
 
+  //In return, change state values to prop values
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={props.store} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+//Pass information from Redux state to the props of App, used in connect
+const mapStateToProps = state => {
+  return {
+    car: state.car,
+    store: state.store,
+    additionalPrice: state.additionalPrice
+  };
+};
+
+//Exports the result of the connect function, using mapStateToProps as a callback, and App as the component
+export default connect(
+  mapStateToProps,
+  {}
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+//import createStore from redux, as it is only affecting redux. This creates the "state" to be stored and passed
+import { createStore } from "redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+//Import Provider from react-redux, as it will be affecting both. This passes the store from Redux to my React App
+import { Provider } from "react-redux";
+
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+//initial setup of state to be used by the redux store
+function reducer() {
+  return {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: "2019 Ford Mustang",
+      image:
+        "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+      features: []
+    },
+    store: [
+      { id: 1, name: "V-6 engine", price: 1500 },
+      { id: 2, name: "Racing detail package", price: 1500 },
+      { id: 3, name: "Premium sound system", price: 500 },
+      { id: 4, name: "Rear spoiler", price: 250 }
+    ]
+  };
+}
+
+//create redux store
+const store = createStore(reducer);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  //passing my redux store to App by wrapping it
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
